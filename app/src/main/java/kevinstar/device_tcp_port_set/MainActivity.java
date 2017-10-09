2cb8a12773a1561661d8b91300881f86d4f15ff7
kevinstar <1097666131@qq.com>
2017-10-09 16:03:39 +0800

    Initial commit 设置Android tcp 监听端口号

---
+++ app/src/main/java/kevinstar/device_tcp_port_set/MainActivity.java
@@ -0,0 +1,37 @@
+package kevinstar.device_tcp_port_set;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.time.Clock;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               new Handler().postDelayed(new Runnable() {
+                   @Override
+                   public void run() {
+                       CommandExecution.CommandResult result= CommandExecution.execCommand(new String[]{"setprop service.adb.tcp.port 5555","stop adbd","start adbd"},false);
+                        if (result.successMsg!=null){
+                            textView.setText("设置成功");
+                        }
+                   }
+               },1000);
+            }
+        });
+    }
+}
